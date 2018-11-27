0e617f2fa8ba4fc80299518436888dfbdee8a1af
magga <adb.dao@gmail.com>
2018-11-27 14:52:29 +0800

    gitk

--- txt.js
+++ txt.js
@@ -2,8 +2,8 @@
 var fs = require('fs')
 var path = require('path')
 // 建立函數，以便回呼使用
-function XmlToMypb(go) {
-    // 規範檔案路徑
+function XmlAddMypb(go) {
+    // 規範化檔案路徑
     var h = path.normalize(go)
     // 取得本檔檔名，以本檔檔名作參數，在當前目錄下：若有相同名的副檔名的檔案，就進行轉換
     // 取得本檔絕對路徑
@@ -18,13 +18,13 @@ function XmlToMypb(go) {
     var d = fs.readdirSync(h)
     // 循環目錄
     for (var k of d) {
-        // 取得絕對路徑，並規範路徑
+        // 取得絕對路徑，並規範化路徑
         var n = path.normalize(h + '/' + k)
         // 取得檔案資訊
         var c = fs.statSync(n)
         // 判斷是否為資料夾，如果是：回呼函數，來執行下一層目錄
         if (c.isDirectory()) {
-            XmlToMypb(n)
+            XmlAddMypb(n)
             // 判斷是否為所要轉換的副檔名的檔案
         } else if (path.extname(n) == ('.' + p.name)) {
 
@@ -32,25 +32,24 @@ function XmlToMypb(go) {
             var a = fs.readFileSync(n, 'utf16LE')
             // 導入陣列
             var b = a.split('\n')
-            // 首行加一空行，後來不必了
-            // b.unshift('\n')
 
+            // 首行加一空行，後來不必了，b[0]之前，不能加任何字，否則「位元組順序記號」 EF BB BF ，會跑到第2行，變成亂碼
+            // b.unshift('\n')
             // 將幾個檔頭位元，改為第一行，後來這方法無效
             // if(/0xEF0xBB0xBF/.test(b[0])){
-            //     b.unshift('﻿')
             // b[0].replace(/[0xEF|0xBB|0xBF]/,'')
+
             // 加上批次頁碼
-            // for (var i = -1, j = 0; i < b.length; i += 20, j++) {
             for (var i = 0, j = 1; i < b.length; i += 20, j++) {
-                b[i] = '<pb n="' + j + '"/>\n' + b[i]
+                // <pb>不能寫在b[0]之前，否則「位元組順序記號」 EF BB BF ，會跑到第2行，變成亂碼
+                b[i] = b[i] + '<pb n="' + j + '"/>'
             }
-            // 位元組順序記號 0a ef bb bf ，會被插入第2行
             // 用絕對路徑寫入檔案
             fs.writeFileSync(n + '.log', b.join('\n'), 'utf8')
             // 完成時返回通知
-            console.log('mypb is OK: ' + k)
+            console.log('mypb is OK: ' + n + '.log')
         }
     }
 }
 // 啟用函數
-XmlToMypb('./')
+XmlAddMypb('./')
