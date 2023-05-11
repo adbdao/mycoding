5cafc5db45cce5b18e4dc0527aa1f97b4d7d6f02
daochung <adb.dao@gmail.com>
2023-05-11 22:58:48 +0800

    pwa add file name

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -16,7 +16,46 @@ self.addEventListener("message", (event) => {
 self.addEventListener('install', async (event) => {
   event.waitUntil(
     caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
+      // .then((cache) => cache.add(offlineFallbackPage))
+      .then((cache) => cache.addAll([
+        //   只要有一個載入失敗，就全部失敗，所以盡量少
+        "0樣本(文檔).html",
+        "0樣本.html",
+        "AccelonLogo2017.html",
+        "bookmarks_link.html",
+        "C3_Example.html",
+        "cbreader Font Big.css",
+        "cbreader Font Middle.css",
+        "cbreader2019.css",
+        "CssClockmore.html",
+        "CssClockred.html",
+        "D3_Example.html",
+        "D3_pack_layout_Tipitaka_History.html",
+        "D3_tree_Vertical_2Kinds.html",
+        "D3_tree_Vertical_Box.html",
+        "D3_tree_Vertical_only.html",
+        "D3_tree_Vertical_Tipitaka_History.html",
+        "index.html",
+        "Lum1.html",
+        "Lum2.html",
+        "README.md",
+        "■正規集.html",
+        "●傳誠手稿字型呈現測試.html",
+        "七月牌位列印.html",
+        "佛七時間表.html",
+        "排班(表格抽籤)2.html",
+        "整月回向文1.html",
+        "日期計算機.html",
+        "誦戒前懺悔1.html",
+        "課誦回向法器板眼.html",
+        "資料庫Link.html",
+        "農曆台灣月曆-佛誕v.html",
+        "農曆台灣月曆-個人月曆.html",
+        "農曆台灣月曆-手機3.html",
+        "農曆台灣月曆-手機4六個月的月曆.html",
+        "農曆台灣月曆-課誦本佛誕日.html",
+        "齋天年齡轉換.html"
+          ]))
   );
 });
 
